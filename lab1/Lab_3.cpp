ba791aba9c0289e104460b5c3a33f874e8c42129
Manaschai2000 <91656011+Manaschai2000@users.noreply.github.com>
2021-09-30 15:23:45 +0700

    Add files via upload

---
+++ lab1/Lab_3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+cout << "Student Code : 64-060214-2124-3 " << endl; 
+cout << "Student Name : Manaschai Phunginn" << endl;
+cout << "Division : Information Technology" << endl;
+cout << "Year : 1" << endl;
+cout << "Room : RB" << endl;
+cout << "Sex : Male" << endl;
+cout << "Age : 21" << endl;
+cout << "NickName : Mix" << endl;
+cout << "Birthday : 19 December 2000" << endl;
+cout << "Favorite food : Beef steak" << endl;
+
+cout<< "================================================================" << endl;
+
+cout << "# #         # #       #######       #           #       "<< endl; 
+cout << "#  #       #  #          #            #       #         "<< endl;
+cout << "#   #     #   #          #              #   #          "<< endl;
+cout << "#    #   #    #          #                #          "<< endl;
+cout << "#     # #     #          #              #   #       "<< endl;
+cout << "#      #      #       #######        #         #     "<< endl;
+return (0);
+}
