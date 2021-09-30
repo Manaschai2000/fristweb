3aeaff4795fa7f4a878972f739162c48ab92de49
Manaschai2000 <6406021421243@fitm.kmutnb.ac.th>
2021-09-30 15:22:07 +0700

    enter

---
+++ lab5/ex/2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int star[5];
+    cout << "enter star : ";
+    cin >> star[5];
+    for(int i=0; i<5;i++){
+    cout << star << endl;
+    }
+    
+}
+
